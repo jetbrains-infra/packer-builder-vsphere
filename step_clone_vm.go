ea812539bcd6ba83c83d095a39e7012199f37c68
LizaTretyakova <elizabet.tretyakova@gmail.com>
2017-05-28 02:22:25 +0300

    removed debug-output

--- step_clone_vm.go
+++ step_clone_vm.go
@@ -34,7 +34,6 @@ func (s *StepCloneVM) Run(state multistep.StateBag) multistep.StepAction {
 	// Prepare entities: client (authentification), finder, folder, virtual machine
 	client, ctx, err := createClient(s.config.Url, s.config.Username, s.config.Password)
 	if err != nil {
-		ui.Say("error creating client")
 		state.Put("error", err)
 		return multistep.ActionHalt
 	}
@@ -43,28 +42,21 @@ func (s *StepCloneVM) Run(state multistep.StateBag) multistep.StepAction {
 	finder := find.NewFinder(client.Client, false)
 	dc, err := finder.DatacenterOrDefault(ctx, s.config.DCName)
 	if err != nil {
-		ui.Say("error exploring datacenter")
 		state.Put("error", err)
 		return multistep.ActionHalt
-	} else {
-		ui.Say(fmt.Sprintf("DC string: %v\nDC path: %v\nDC name: %v", dc.String(), dc.InventoryPath, dc.Name()))
 	}
 	finder.SetDatacenter(dc)
 
 	// Get folder
 	folder, err := finder.FolderOrDefault(ctx, s.config.FolderName)
 	if err != nil {
-		ui.Say("error creating folder")
 		state.Put("error", err)
 		return multistep.ActionHalt
-	} else {
-		ui.Say(fmt.Sprintf("Folder string: %v\nFolder path: %v\nFolder name: %v", folder.String(), folder.InventoryPath, folder.Name()))
 	}
 
 	// Get resource pool
 	pool, err := finder.ResourcePoolOrDefault(ctx, fmt.Sprintf("/%v/host/%v/Resources/%v", dc.Name(), s.config.Host, s.config.ResourcePool))
 	if err != nil {
-		ui.Error("error obtaining reasource pool")
 		state.Put("error", err)
 		return multistep.ActionHalt
 	}
@@ -74,7 +66,6 @@ func (s *StepCloneVM) Run(state multistep.StateBag) multistep.StepAction {
 	if s.config.Datastore != "" {
 		datastore, err = finder.Datastore(ctx, s.config.Datastore)
 		if err != nil {
-			ui.Say("Cannot find datastore datastore2")
 			state.Put("error", err)
 			return multistep.ActionHalt
 		}
@@ -83,7 +74,6 @@ func (s *StepCloneVM) Run(state multistep.StateBag) multistep.StepAction {
 	// Get source VM
 	vmSrc, err := finder.VirtualMachine(ctx, s.config.Template)
 	if err != nil {
-		ui.Say("error creating vmSrc")
 		state.Put("error", err)
 		return multistep.ActionHalt
 	}
@@ -91,13 +81,12 @@ func (s *StepCloneVM) Run(state multistep.StateBag) multistep.StepAction {
 	vm, err := cloneVM(&CloneParameters{
 		client:          client,
 		folder:          folder,
-		//hostRef:         host.Reference(),
 		resourcePool: pool,
 		datastore:    datastore,
 		vmSrc:        vmSrc,
 		ctx:          ctx,
 		vmName:       s.config.VMName,
-	}, ui)
+	})
 	if err != nil {
 		state.Put("error", err)
 		return multistep.ActionHalt
@@ -137,13 +126,12 @@ func (s *StepCloneVM) Cleanup(state multistep.StateBag) {
 	}
 }
 
-func cloneVM(params *CloneParameters, ui packer.Ui) (vm *object.VirtualMachine, err error) {
+func cloneVM(params *CloneParameters) (vm *object.VirtualMachine, err error) {
 	vm = nil
 	err = nil
 	poolRef := params.resourcePool.Reference()
 
 	// Creating specs for cloning
-	ui.Say("Creating specs")
 	relocateSpec := types.VirtualMachineRelocateSpec{
 		Pool: &(poolRef),
 	}
@@ -158,25 +146,17 @@ func cloneVM(params *CloneParameters, ui packer.Ui) (vm *object.VirtualMachine, 
 	}
 
 	// Cloning itself
-	ui.Say("Cloning itself")
 	task, err := params.vmSrc.Clone(params.ctx, params.folder, params.vmName, cloneSpec)
 	if err != nil {
-		ui.Error(fmt.Sprintf("error from Clone(): %v", err.Error()))
 		return
 	}
 
-	ui.Say(fmt.Sprint("Task: %v", task))
-
-	ui.Say("Wait for result")
 	info, err := task.WaitForResult(params.ctx, nil)
 	if err != nil {
-		ui.Error(fmt.Sprintf("error from WaitForResult(): %v\ninfo: %v", err.Error(), info))
 		return
 	}
 
-	ui.Say("Before creating vm")
 	vm = object.NewVirtualMachine(params.client.Client, info.Result.(types.ManagedObjectReference))
-	ui.Say("After creating VM")
 	return vm, nil
 }
 
