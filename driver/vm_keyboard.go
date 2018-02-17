5b10843a8aad3aa91853709be030f81ebf88f42d
Michael Kuzmin <mkuzmin@gmail.com>
2018-02-18 02:39:18 +0300

    refactor

--- driver/vm_keyboard.go
+++ driver/vm_keyboard.go
@@ -2,152 +2,81 @@ package driver
 
 import (
 	"strings"
-	"unicode/utf8"
 	"unicode"
 	"github.com/vmware/govmomi/vim25/types"
 	"github.com/vmware/govmomi/vim25/methods"
+	"golang.org/x/mobile/event/key"
 )
 
-func (vm *VirtualMachine) PutUsbScanCodes(s string) (int32, error) {
-	spec := &types.PutUsbScanCodes{
-		This: vm.vm.Reference(),
-		Spec: getUsbScanCodeSpec(s),
+type KeyInput struct {
+	Message  string
+	Scancode key.Code
+	Alt      bool
+	Ctrl     bool
+	Shift    bool
+}
+
+var scancodeMap = make(map[rune]key.Code)
+
+func init() {
+	scancodeIndex := make(map[string]key.Code)
+	scancodeIndex["abcdefghijklmnopqrstuvwxyz"] = key.CodeA
+	scancodeIndex["ABCDEFGHIJKLMNOPQRSTUVWXYZ"] = key.CodeA
+	scancodeIndex["1234567890"] = key.Code1
+	scancodeIndex["!@#$%^&*()"] = key.Code1
+	scancodeIndex[" "] = key.CodeSpacebar
+	scancodeIndex["-=[]\\"] = key.CodeHyphenMinus
+	scancodeIndex["_+{}|" ] = key.CodeHyphenMinus
+	scancodeIndex[ ";'`,./" ] = key.CodeSemicolon
+	scancodeIndex[":\"~<>?" ] = key.CodeSemicolon
+
+	for chars, start := range scancodeIndex {
+		for i, r := range chars {
+			scancodeMap[r] = start + key.Code(i)
+		}
 	}
-	resp, err := methods.PutUsbScanCodes(vm.driver.ctx, vm.driver.client.RoundTripper, spec)
+}
+
+const shiftedChars = "!@#$%^&*()_+{}|:\"~<>?"
+
+func (vm *VirtualMachine) TypeOnKeyboard(input KeyInput) (int32, error) {
+	var spec types.UsbScanCodeSpec
+
+	for _, r := range input.Message {
+		scancode := scancodeMap[r]
+		shift := input.Shift || unicode.IsUpper(r) || strings.ContainsRune(shiftedChars, r)
+
+		spec.KeyEvents = append(spec.KeyEvents, types.UsbScanCodeSpecKeyEvent{
+			// https://github.com/lamw/vghetto-scripts/blob/f74bc8ba20064f46592bcce5a873b161a7fa3d72/powershell/VMKeystrokes.ps1#L130
+			UsbHidCode: int32(scancode)<<16 | 7,
+			Modifiers: &types.UsbScanCodeSpecModifierType{
+				LeftControl: &input.Ctrl,
+				LeftAlt:     &input.Alt,
+				LeftShift:   &shift,
+			},
+		})
+	}
+
+	if input.Scancode != 0 {
+		spec.KeyEvents = append(spec.KeyEvents, types.UsbScanCodeSpecKeyEvent{
+			UsbHidCode: int32(input.Scancode)<<16 | 7,
+			Modifiers: &types.UsbScanCodeSpecModifierType{
+				LeftControl: &input.Ctrl,
+				LeftAlt:     &input.Alt,
+				LeftShift:   &input.Shift,
+			},
+		})
+	}
+
+	req := &types.PutUsbScanCodes{
+		This: vm.vm.Reference(),
+		Spec: spec,
+	}
+
+	resp, err := methods.PutUsbScanCodes(vm.driver.ctx, vm.driver.client.RoundTripper, req)
 	if err != nil {
 		return 0, err
 	}
 
 	return resp.Returnval, nil
-}
-
-func getUsbScanCodeSpec(message string) types.UsbScanCodeSpec {
-	scancodeIndex := make(map[string]uint)
-	scancodeIndex["abcdefghijklmnopqrstuvwxyz"] = 4
-	scancodeIndex["ABCDEFGHIJKLMNOPQRSTUVWXYZ"] = 4
-	scancodeIndex["1234567890"] = 30
-	scancodeIndex["!@#$%^&*()"] = 30
-	scancodeIndex[" "] = 44
-	scancodeIndex["-=[]\\"] = 45
-	scancodeIndex["_+{}|" ] = 45
-	scancodeIndex[ ";'`,./" ] = 51
-	scancodeIndex[":\"~<>?" ] = 51
-
-	shiftedChars := "!@#$%^&*()_+{}|:\"~<>?"
-
-	scancodeMap := make(map[rune]uint)
-	for chars, start := range scancodeIndex {
-		var i uint = 0
-		for len(chars) > 0 {
-			r, size := utf8.DecodeRuneInString(chars)
-			chars = chars[size:]
-			scancodeMap[r] = start + i
-			i += 1
-		}
-	}
-
-	special := map[string]uint{
-		"<enter>":    40,
-		"<esc>":      41,
-		"<bs>":       42,
-		"<del>":      76,
-		"<tab>":      43,
-		"<f1>":       58,
-		"<f2>":       59,
-		"<f3>":       60,
-		"<f4>":       61,
-		"<f5>":       62,
-		"<f6>":       63,
-		"<f7>":       64,
-		"<f8>":       65,
-		"<f9>":       66,
-		"<f10>":      67,
-		"<f11>":      68,
-		"<f12>":      69,
-		"<insert>":   73,
-		"<home>":     74,
-		"<end>":      77,
-		"<pageUp>":   75,
-		"<pageDown>": 78,
-		"<left>":     80,
-		"<right>":    79,
-		"<up>":       82,
-		"<down>":     81,
-	}
-
-	spec := types.UsbScanCodeSpec{
-		KeyEvents: []types.UsbScanCodeSpecKeyEvent{},
-	}
-
-	var keyAlt bool
-	var keyCtrl bool
-	var keyShift bool
-
-	for len(message) > 0 {
-		var scancode uint
-
-		if strings.HasPrefix(message, "<leftAltOn>") {
-			keyAlt = true
-			message = message[len("<leftAltOn>"):]
-		}
-
-		if strings.HasPrefix(message, "<leftAltOff>") {
-			keyAlt = false
-			message = message[len("<leftAltOff>"):]
-		}
-
-		if strings.HasPrefix(message, "<leftCtrlOn>") {
-			keyCtrl = true
-			message = message[len("<leftCtrlOn>"):]
-		}
-
-		if strings.HasPrefix(message, "<leftCtrlOff>") {
-			keyCtrl = false
-			message = message[len("<leftCtrlOff>"):]
-		}
-
-		if strings.HasPrefix(message, "<leftShiftOn>") {
-			keyShift = true
-			message = message[len("<leftShiftOn>"):]
-		}
-
-		if strings.HasPrefix(message, "<leftShiftOff>") {
-			keyShift = false
-			message = message[len("<leftShiftOff>"):]
-		}
-
-		if scancode == 0 {
-			for specialCode, specialValue := range special {
-				if strings.HasPrefix(message, specialCode) {
-					scancode = specialValue
-					message = message[len(specialCode):]
-					break
-				}
-			}
-		}
-
-		modAlt := keyAlt
-		modCtrl := keyCtrl
-		modShift := keyShift
-
-		if scancode == 0 {
-			r, size := utf8.DecodeRuneInString(message)
-			scancode = scancodeMap[r]
-			modShift = modShift || unicode.IsUpper(r) || strings.ContainsRune(shiftedChars, r)
-			message = message[size:]
-		}
-
-		event := types.UsbScanCodeSpecKeyEvent{
-			// https://github.com/lamw/vghetto-scripts/blob/f74bc8ba20064f46592bcce5a873b161a7fa3d72/powershell/VMKeystrokes.ps1#L130
-			UsbHidCode: int32(scancode)<<16 | 7,
-			Modifiers: &types.UsbScanCodeSpecModifierType{
-				LeftAlt:     &modAlt,
-				LeftControl: &modCtrl,
-				LeftShift:   &modShift,
-			},
-		}
-		spec.KeyEvents = append(spec.KeyEvents, event)
-	}
-
-	return spec
 }
