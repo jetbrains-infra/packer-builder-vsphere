61a54ee9b7de6e037b3e26e71136d85bf5fc6f3b
remijouannet <remijouannet@gmail.com>
2018-10-17 01:41:11 +0200

    fix indent

--- test.sh
+++ test.sh
@@ -6,12 +6,13 @@ export PACKER_ACC=1
 
 RETVAL=0
 
-for file in $(find ./ -type f ! -path "./vendor/*" -name '*.go' -print); do
-	if [[ -n "$(gofmt -l "$file")" ]]
+for file in $(find ./ -type f ! -path "./vendor/*" -name '*.go' -print)
+do
+    if [[ -n "$(gofmt -l "$file")" ]]
     then
-		echo -e "$file does not conform to gofmt rules. Run: gofmt -s -w $file"
-		RETVAL=1
-	fi
+        echo -e "$file does not conform to gofmt rules. Run: gofmt -s -w $file"
+        RETVAL=1
+    fi
 done
 
 exit $RETVAL
